a27f4c0692539e3a7cc317dd7b9617cff97bbe9a
adrianaabad2004 <adrianaabad2014@gmail.com>
2022-02-16 19:42:54 -0400

    Primer commit

---
+++ Principal/Contratar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Principal
+{
+    class Contratar
+    {
+        public Contratar()
+        {
+        }
+
+        public void Contratar_Empleado(List<Empleado> list)
+        {
+            Empleado e = new Empleado();
+
+            Console.Write("Cedula: ");
+            e.cedula = Console.ReadLine();
+            Console.Write("Nombre: ");
+            e.nombre = Console.ReadLine();
+            Console.Write("Departamento: ");
+            e.departamento = Console.ReadLine();
+            Console.Write("Cargo: ");
+            e.cargo = Console.ReadLine();
+            Console.Write("Salario: ");
+            e.salario = double.Parse(Console.ReadLine());
+
+            list.Add(e);
+        }
+    }
+}
